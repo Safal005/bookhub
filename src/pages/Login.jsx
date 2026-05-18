313e65ae96db5f9d50f1cedee24086f324e71c74
Safal005 <khanalsafal5@gmail.com>
2026-05-18 17:33:35 +0545

    feat: implement persistent authentication logic using localStorage and useNavigate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,46 @@
 import {useState} from 'react';
 import '../pages/Auth.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 function Login(){
     
-    const [showPassword, setShowPassword]=useState(false);
-    
-    
+    const [showPassword, setShowPassword] = useState(false);
+    const [password, setPassword] = useState();
+    const [username, setUsername] = useState();
+    const navigate = useNavigate();
+
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        const savedData = localStorage.getItem("registeredUser");
+        if(savedData){
+            const user= JSON.parse (savedData);
+            if (user.username==username && user.password==password){
+                alert('Login Successful!');
+                navigate('/Dashboard');
+            }
+            else{
+                alert("Incorrect username and password!");
+            }
+        }
+        else
+            alert('No user found. Please Sign Up first!');
+    };
     return(
         <>
             <div className='login-wrap'>
 
-                <form className='login-container' onSubmit={(e) => e.preventDefault()}>
+                <form className='login-container' onSubmit={handleLogin}>
                     
                     <h1><i className="fa-solid fa-right-to-bracket"></i> Sign-in</h1>
 
                     <div className='input-group'>
                         <label htmlFor="username" className='user-pass'>Username</label>
-                        <input type="text" placeholder='Username' className='input-field' required/>
+                        <input type="text" placeholder='Username' className='input-field' value={username} onChange={(e) => setUsername(e.target.value)} required/>
                     </div>
 
                     <div className='input-group'>
                         <label htmlFor="password" className='user-pass'>Password</label>
                         <div className='password-input-wrapper'>
-                            <input type={showPassword? "text":"password"} placeholder='Password' className='input-field' required/>
+                            <input type={showPassword? "text":"password"} placeholder='Password' className='input-field' value={password} onChange={(e) => setPassword(e.target.value)} required/>
                             <i className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"} password-toggle-icon`} onClick={() => setShowPassword(!showPassword)}></i>
                     </div>
                     </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,19 +1,37 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 function SignUp(){
-    const [showPassword, setShowPassword]=useState(false);
+    const [showPassword, setShowPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
+    const navigate=useNavigate();
+    const handleRegister =(e)=>{
+        e.preventDefault();
+        if(password!==confirmPassword){
+            alert('Passwords do not match!');
+            return;
+        }
+        const userData={
+            username:username,
+            password:password
+        };
+        localStorage.setItem("registeredUser",JSON.stringify(userData));
+        alert("Registration Successfull!")
+        navigate("/");
+    }
     return(
         <>
             <div className='login-wrap'>
 
-                <form className='login-container' onSubmit={(e) => e.preventDefault()}>
+                <form className='login-container' onSubmit={handleRegister}>
                     <h1><i className="fa-solid fa-user-plus"></i> Sign-Up</h1>
 
                     <div className='input-group'>
                         <label htmlFor="username" className='user-pass'>Username</label>
-                        <input type="text" placeholder='Username' className='input-field' required/>
+                        <input type="text" placeholder='Username' className='input-field' value={username} onChange={(e)=> setUsername(e.target.value)} required/>
                     </div>
 
                     <div className='input-group'>
@@ -24,7 +42,7 @@ function SignUp(){
                     <div className='input-group'>
                         <label htmlFor="password" className='user-pass'>Password</label>
                         <div className='password-input-wrapper'>
-                            <input type={showPassword? "text":"password"} placeholder='Password' className='input-field' required/>
+                            <input type={showPassword? "text":"password"} placeholder='Password' className='input-field' value={password} onChange={(e) => setPassword(e.target.value)} required/>
                             <i className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"} password-toggle-icon`} onClick={() => setShowPassword(!showPassword)}></i>
                     </div>
                     </div>
@@ -32,7 +50,7 @@ function SignUp(){
                     <div className='input-group'>
                         <label htmlFor="password" className='user-pass'>Confirm Password</label>
                         <div className='password-input-wrapper'>
-                            <input type={showConfirmPassword? "text":"password"} placeholder='Confirm Password' className='input-field' required/>
+                            <input type={showConfirmPassword? "text":"password"} placeholder='Confirm Password' className='input-field' value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)} required/>
                             <i className={`fa-solid ${showConfirmPassword ? "fa-eye-slash" : "fa-eye"} password-toggle-icon`} onClick={() => setShowConfirmPassword(!showConfirmPassword)}></i>
                     </div>
                     </div>
